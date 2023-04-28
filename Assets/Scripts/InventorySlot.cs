ccc6b5dc951444059a1a7e952c0dd2f8af05973f
Rowan Martel <92890554+NotEnoughUwU@users.noreply.github.com>
2023-04-27 22:19:42 -0300

    Fixed item slots

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 using UnityEngine.UIElements;
 
 public class InventorySlot : VisualElement
@@ -14,6 +15,24 @@ public class InventorySlot : VisualElement
         Add(Icon);
         //Add USS style properties to the elements
         Icon.AddToClassList("slotIcon");
-        AddToClassList("slotContainer");
+        AddToClassList("slotContainer2");
     }
+
+    public void HoldItem(ItemDetails item)
+    {
+        Icon.image = item.Icon.texture;
+        ItemGuid = item.GUID;
+    }
+    public void DropItem()
+    {
+        ItemGuid = "";
+        Icon.image = null;
+    }
+
+    #region UXML
+    [Preserve]
+    public new class UxmlFactory : UxmlFactory<InventorySlot, UxmlTraits> { }
+    [Preserve]
+    public new class UxmlTraits : VisualElement.UxmlTraits { }
+    #endregion
 }
